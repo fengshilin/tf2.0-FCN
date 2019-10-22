ff7451e736b76c0776f539c653486cb026d6b57d
fengshilin <51773634+fengshilin@users.noreply.github.com>
2019-10-22 12:52:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-train_dir = 'data/train/img/'
-train_label_dir = 'data/train/label/'
-test_dir = 'data/test/'
-weight_path = "weights/"
+train_dir = 'data/train/img/'  # 训练集目录
+train_label_dir = 'data/train/label/'  # 训练集对应的标签目录
+test_dir = 'data/test/'  # 测试集目录
+weight_path = "weights/"  # 参数放置目录
 
-num_epochs = 50
-learning_rate = 0.0001
-image_shape = (160, 576)
-batch_size=10
+num_epochs = 50  # 训练轮数
+learning_rate = 0.0001  # 学习率
+image_shape = (160, 576)  # resize到的图片长宽
+batch_size=10  # 批量
